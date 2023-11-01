5d5c22b1c693a8e357be5acd1196595832d803eb
Dasha2501 <dasaakimenko860@gmail.com>
2023-11-01 20:28:29 +0200

    Login pages

--- js/main.js
+++ js/main.js
@@ -1,110 +1,128 @@
 const positionNav = () => {
-	const headerEl = document.querySelector('.header');
-	const navEl = document.querySelector('.nav');
-	const headerHeight = headerEl.offsetHeight;
-
-	navEl.style.top = headerHeight - 1 + 'px';
-};
-
-const openNavigation = () => {
-	const burgerNavEl = document.querySelector('.burgerNav');
-
-	burgerNavEl.addEventListener('click', () => {
-		document.querySelector('.nav').classList.toggle('visible');
-		burgerNavEl.classList.toggle('open');
-	});
-};
-
-const searchBooksInHeader = () => {
-	const fieldSearch = document.querySelector('.headerBody .fieldSearchBook');
-	const searchOptions = document.querySelector(
-		'.headerBody .searchBlockOptions'
-	);
-	const searchElements = document.querySelectorAll(
-		'.headerBody .searchBlockOption'
-	);
-
-	fieldSearch.addEventListener(
-		'click',
-		() => (searchOptions.style.display = 'flex')
-	);
-
-	searchElements.forEach(book => {
-		book.addEventListener('click', () => {
-			fieldSearch.value = book.textContent.trim();
-			searchOptions.style.display = 'none';
-		});
-	});
-
-	fieldSearch.addEventListener('input', () => {
-		const inputValue = fieldSearch.value.toLowerCase();
-
-		searchElements.forEach(book => {
-			const bookName = book.textContent.toLowerCase();
-
-			bookName.includes(inputValue)
-				? (book.style.display = 'flex')
-				: (book.style.display = 'none');
-		});
-	});
-
-	document.addEventListener('click', event => {
-		!fieldSearch.contains(event.target) && !searchOptions.contains(event.target)
-			? (searchOptions.style.display = 'none')
-			: null;
-	});
-};
-
-const searchBooksOutHeader = () => {
-	const fieldSearch = document.querySelector('.fieldSearchBookOut');
-	const searchOptions = document.querySelector('.searchBlockOutOptions');
-	const searchElements = document.querySelectorAll('.searchBlockOutOption');
-
-	fieldSearch.addEventListener(
-		'click',
-		() => (searchOptions.style.display = 'flex')
-	);
-
-	searchElements.forEach(book => {
-		book.addEventListener('click', () => {
-			fieldSearch.value = book.textContent.trim();
-			searchOptions.style.display = 'none';
-		});
-	});
-
-	fieldSearch.addEventListener('input', () => {
-		const inputValue = fieldSearch.value.toLowerCase();
-
-		searchElements.forEach(book => {
-			const bookName = book.textContent.toLowerCase();
-
-			bookName.includes(inputValue)
-				? (book.style.display = 'flex')
-				: (book.style.display = 'none');
-		});
-	});
-
-	document.addEventListener('click', event => {
-		!fieldSearch.contains(event.target) && !searchOptions.contains(event.target)
-			? (searchOptions.style.display = 'none')
-			: null;
-	});
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-	openNavigation();
-	searchBooksInHeader();
-	searchBooksOutHeader();
-	positionNav();
-});
-
-window.addEventListener('resize', positionNav);
-
-$('.sliderBlock').slick({
-	slidesToScroll: 1,
-	dots: false,
-	arrows: false,
-	adaptiveHeight: true,
-	autoplay: true,
-	autoplaySpeed: 1000,
-});
+    const headerEl = document.querySelector('.header');
+    const navEl = document.querySelector('.nav');
+    const headerHeight = headerEl.offsetHeight;
+  
+    navEl.style.top = headerHeight - 1 + 'px';
+  };
+  
+  const openNavigation = () => {
+    const burgerNavEl = document.querySelector('.burgerNav');
+  
+    burgerNavEl.addEventListener('click', () => {
+      document.querySelector('.nav').classList.toggle('visible');
+      burgerNavEl.classList.toggle('open');
+    });
+  };
+  
+  const searchBooksInHeader = () => {
+    const fieldSearch = document.querySelector('.headerBody .fieldSearchBook');
+    const searchOptions = document.querySelector(
+      '.headerBody .searchBlockOptions'
+    );
+    const searchElements = document.querySelectorAll(
+      '.headerBody .searchBlockOption'
+    );
+  
+    fieldSearch.addEventListener(
+      'click',
+      () => (searchOptions.style.display = 'flex')
+    );
+  
+    searchElements.forEach(book => {
+      book.addEventListener('click', () => {
+        fieldSearch.value = book.textContent.trim();
+        searchOptions.style.display = 'none';
+      });
+    });
+  
+    fieldSearch.addEventListener('input', () => {
+      const inputValue = fieldSearch.value.toLowerCase();
+  
+      searchElements.forEach(book => {
+        const bookName = book.textContent.toLowerCase();
+  
+        bookName.includes(inputValue)
+          ? (book.style.display = 'flex')
+          : (book.style.display = 'none');
+      });
+    });
+  
+    document.addEventListener('click', event => {
+      !fieldSearch.contains(event.target) && !searchOptions.contains(event.target)
+        ? (searchOptions.style.display = 'none')
+        : null;
+    });
+  };
+  
+  const searchBooksOutHeader = () => {
+    const fieldSearch = document.querySelector('.fieldSearchBookOut');
+    const searchOptions = document.querySelector('.searchBlockOutOptions');
+    const searchElements = document.querySelectorAll('.searchBlockOutOption');
+  
+    fieldSearch.addEventListener(
+      'click',
+      () => (searchOptions.style.display = 'flex')
+    );
+  
+    searchElements.forEach(book => {
+      book.addEventListener('click', () => {
+        fieldSearch.value = book.textContent.trim();
+        searchOptions.style.display = 'none';
+      });
+    });
+  
+    fieldSearch.addEventListener('input', () => {
+      const inputValue = fieldSearch.value.toLowerCase();
+  
+      searchElements.forEach(book => {
+        const bookName = book.textContent.toLowerCase();
+  
+        bookName.includes(inputValue)
+          ? (book.style.display = 'flex')
+          : (book.style.display = 'none');
+      });
+    });
+  
+    document.addEventListener('click', event => {
+      !fieldSearch.contains(event.target) && !searchOptions.contains(event.target)
+        ? (searchOptions.style.display = 'none')
+        : null;
+    });
+  };
+  
+  const searchAuthours = () => {
+    const fieldSearch = document.querySelector('.fieldSearchAuthours');
+    const searchElements = document.querySelectorAll('.searchAuthoursOption');
+  
+    fieldSearch.addEventListener('input', () => {
+      const inputValue = fieldSearch.value.toLowerCase();
+  
+      searchElements.forEach(authour => {
+        const authourName = authour.textContent.toLowerCase();
+  
+        authourName.includes(inputValue)
+          ? (authour.style.display = 'block')
+          : (authour.style.display = 'none');
+      });
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    positionNav();
+    openNavigation();
+    searchBooksInHeader();
+    searchBooksOutHeader();
+    searchAuthours();
+  });
+  
+  window.addEventListener('resize', positionNav);
+  
+  $('.sliderBlock').slick({
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
